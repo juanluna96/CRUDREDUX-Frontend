8b9c95e096fd4a4319f293278e1b701de2661f89
juanluna <juandluna17@gmail.com>
2021-04-06 09:07:22 -0500

    296. Creando la Edición de Productos y Listado

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Productos = () => {
     return (
-        <h1 className="mt-3">Desde productos.js</h1>
+        <Fragment>
+            <h2 className="my-5 text-center">Listado de productos</h2>
+            <table className="table table-light">
+                <thead className="thead-light">
+                    <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Producto 1</td>
+                        <td>20.000</td>
+                        <td>Acciones</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Fragment>
     )
 }
 
