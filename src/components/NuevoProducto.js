c417f2822b2cdec2f801084cfeef09f0a48753da
juanluna <juandluna17@gmail.com>
2021-04-05 20:50:10 -0500

    295. Creando el Formulario para Nuevos Productos

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -2,8 +2,27 @@ import React from 'react'
 
 const NuevoProducto = () => {
     return (
-        <div>
-            <h1 className="mt-5">Desde nuevo producto</h1>
+        <div className="container mt-5 row justify-content-center">
+            <div className="col-md-5">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="mb-4 text-center card-title font-weight-bold text-uppercase">Agregar nuevo producto</h5>
+                        <p className="card-text">
+                            <form>
+                                <div className="form-group">
+                                    <label htmlFor="nombre">Nombre del producto</label>
+                                    <input className="form-control" type="text" name="nombre" />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="precio">Precio</label>
+                                    <input className="form-control" type="text" name="precio" />
+                                </div>
+                                <button className="btn btn-primary font-weight-bold text-uppercase d-block w-100" type="submit">Agregar</button>
+                            </form>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
