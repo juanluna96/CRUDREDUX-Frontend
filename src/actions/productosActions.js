ce903394e40d86a4f64d3ca57a31902b91ddc53c
juanluna <juandluna17@gmail.com>
2021-04-11 16:26:39 -0500

    307. Configurando El Action

--- src/actions/productosActions.js
+++ src/actions/productosActions.js
@@ -1,4 +1,9 @@
-import { AGREGAR_PRODUCTO, AGREGAR_PRODUCTO_ERROR, AGREGAR_PRODUCTO_EXITO } from '../types';
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_ERROR,
+    AGREGAR_PRODUCTO_EXITO,
+    COMENZAR_DESCARGA_PRODUCTOS
+} from '../types';
 import { clienteAxios } from '../config/axios';
 import Swal from 'sweetalert2';
 
@@ -55,5 +60,20 @@ const agregarProductoError = (estado) => {
     return ({
         type: AGREGAR_PRODUCTO_ERROR,
         payload: estado
+    })
+}
+
+// Función que descarga los productos de la base de datos
+
+export function obtenerProductosAction() {
+    return async function (dispatch) {
+        dispatch(descargarProductos())
+    }
+}
+
+const descargarProductos = () => {
+    return ({
+        type: COMENZAR_DESCARGA_PRODUCTOS,
+        payload: true
     })
 }
--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -1,4 +1,9 @@
-import { AGREGAR_PRODUCTO, AGREGAR_PRODUCTO_ERROR, AGREGAR_PRODUCTO_EXITO } from '../types';
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_ERROR,
+    AGREGAR_PRODUCTO_EXITO,
+    COMENZAR_DESCARGA_PRODUCTOS
+} from '../types';
 
 // Cada reducer tiene su propio state
 const initialState = {
@@ -9,6 +14,7 @@ const initialState = {
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
+        case COMENZAR_DESCARGA_PRODUCTOS:
         case AGREGAR_PRODUCTO:
             return { ...state, loading: payload }
         case AGREGAR_PRODUCTO_EXITO:
