3e14f9929ec1edcab9175d0bbc2e39b665fba05e
juanluna <juandluna17@gmail.com>
2021-04-11 20:33:00 -0500

    311. Mostrando errores y texto de cargando...

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -4,6 +4,7 @@ import React, { Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { obtenerProductosAction } from '../actions/productosActions';
 import Producto from './Producto';
+import Spinner from './Spinner/Spinner';
 
 const Productos = () => {
 
@@ -19,10 +20,14 @@ const Productos = () => {
 
     // Obtener el state
     const productos = useSelector(state => state.productos.productos)
+    const error = useSelector(state => state.productos.error);
+    const cargando = useSelector(state => state.productos.loading);
 
     return (
         <Fragment>
             <h2 className="my-5 text-center">Listado de productos</h2>
+            {error ? <p className="my-2 text-center font-weight-bold alert alert-danger">Hubo un error</p> : null }
+            {cargando ? <Spinner></Spinner> : null }
             <table className="table table-light table-striped">
                 <thead className="thead-light">
                     <tr>
@@ -32,7 +37,7 @@ const Productos = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    { productos.lenght === 0 ? 'No hay productos' : (
+                    { productos.length === 0 ? (<tr><td colspan="3" className="text-center">No hay productos</td></tr>) : (
                         productos.map((producto) => {
                             return (<Producto key={ producto.id } producto={ producto }></Producto>)
                         })
