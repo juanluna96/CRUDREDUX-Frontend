7043dd2ccbb3173a145bda9156830f3271937605
juanluna <juandluna17@gmail.com>
2021-04-13 11:25:57 -0500

    319. Leyendo los nuevos valores

--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NumberFormat from 'react-number-format';
 import { useDispatch, useSelector } from 'react-redux';
 import { editarProductoAction } from '../actions/productosActions';
 
 const EditarProducto = ({ history }) => {
+    // Nuevo state de producto
+    const [producto, setProducto] = useState({
+        nombre: '',
+        precio: 0
+    });
+
+    const productoeditar = useSelector(state => state.productos.productoeditar);
+
+
+    // Llenar el state automáticamente
+    useEffect(() => {
+        setProducto(productoeditar);
+    }, [productoeditar])
+
     const dispatch = useDispatch();
-    const producto = useSelector(state => state.productos.productoeditar);
 
     if (!producto) {
         // Redireccionar al home
         history.push('/');
         return null;
     }
+
+    // Leer los datos del formulario
+    const onChangeFormulario = (e) => {
+        setProducto({
+            ...producto,
+            [e.target.name]: e.target.value
+        })
+    }
+
 
     const onSubmitEditarProducto = (e) => {
         e.preventDefault();
@@ -28,11 +50,11 @@ const EditarProducto = ({ history }) => {
                         <form onSubmit={ onSubmitEditarProducto }>
                             <div className="form-group">
                                 <label htmlFor="nombre">Nombre del producto</label>
-                                <input value={ producto.nombre } className="form-control" type="text" name="nombre" />
+                                <input value={ producto.nombre } className="form-control" type="text" name="nombre" onChange={ onChangeFormulario } />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="precio">Precio</label>
-                                <NumberFormat isNumericString='true' value={ producto.precio } className="form-control" name="precio" id="precio" thousandSeparator={ true } prefix={ 'COP $' } />
+                                <NumberFormat isNumericString='true' value={ producto.precio } className="form-control" name="precio" id="precio" thousandSeparator={ true } prefix={ 'COP $' } onChange={ onChangeFormulario } />
                             </div>
                             <button className="btn btn-primary font-weight-bold text-uppercase d-block w-100" type="submit">Guardar cambios</button>
                         </form>
